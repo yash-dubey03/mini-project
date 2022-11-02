8fb2778f881e27b020dfe2ffca98857d8099c0f8
ΛYUSH <82326586+Siddzzzz@users.noreply.github.com>
2022-11-02 09:48:41 +0530

    Update project.py

--- project.py
+++ project.py
@@ -43,3 +43,39 @@ layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 ret = True
+
+while ret:
+
+    ret, img = cap.read()
+    img = imutils.resize(img,height=500)
+    # img = cv2.imread('test.png')
+    height, width = img.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    confidences = []
+    boxes = []
+    classIds = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.3:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                classIds.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
